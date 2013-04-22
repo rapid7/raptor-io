34c206e632cf2bf83e9df6ab1a1dd6314a4b96f6
Tasos Laskos <Tasos_Laskos@rapid7.com>
2013-04-22 23:54:10 +0300

    Added API and specs for Protocol::HTTP::Client

--- lib/raptor/protocol/http/client.rb
+++ lib/raptor/protocol/http/client.rb
@@ -1,3 +1,5 @@
+require 'thread' # we want Queue
+
 module Raptor
 module Protocol::HTTP
 
@@ -7,6 +9,95 @@ module Protocol::HTTP
 # @author Tasos Laskos <tasos_laskos@rapid7.com>
 #
 class Client
+
+  # @return [String]  Address of the HTTP server.
+  attr_reader :address
+
+  # @return [Integer]  Port number of the HTTP server.
+  attr_reader :port
+
+  #
+  # @param  [Hash]  options Request options.
+  # @option options [String] :address Address of the HTTP server.
+  # @option options [Integer] :port (80) Port number of the HTTP server.
+  # @option options [Integer] :port (80) Port number of the HTTP server.
+  #
+  def initialize( options )
+    options.each do |k, v|
+      begin
+        send( "#{k}=", v )
+      rescue NoMethodError
+        instance_variable_set( "@#{k}".to_sym, v )
+      end
+    end
+
+    fail ArgumentError, "Missing ':address' option." if !@address
+
+    @port ||= 80
+    @queue = Queue.new
+  end
+
+  #
+  # Creates and {#queue queues} a {Request}.
+  #
+  # @param  [String]  url URL of the remote resource.
+  # @param  [Hash]  options {Request} options.
+  # @param  [Block] block Callback to be passed the {Response}.
+  #
+  # @return [Request] Queued {Request}.
+  #
+  # @see Request#initialize
+  # @see Request#on_complete
+  # @see #queue
+  #
+  def request( url, options = {}, &block )
+    req = Request.new( options.merge( url: url ) )
+    req.on_complete( &block ) if block_given?
+    queue( req )
+    req
+  end
+
+  #
+  # Creates and {#queue queues} a GET {Request}.
+  #
+  # @param (see #request)
+  # @return (see #request)
+  #
+  def get( url, options = {}, &block )
+    request( url, options.merge( http_method: :get ), &block )
+  end
+
+  #
+  # Creates and {#queue queues} a POST {Request}.
+  #
+  # @param (see #request)
+  # @return (see #request)
+  #
+  def post( url, options = {}, &block )
+    request( url, options.merge( http_method: :post ), &block )
+  end
+
+  # @return [Integer] The amount of {#queue queued} requests.
+  def queue_size
+    @queue.size
+  end
+
+  #
+  # Queues a {Request}.
+  #
+  # @param  [Request] request
+  # @return [Request] `request`
+  #
+  def queue( request )
+    @queue << request
+    request
+  end
+  alias :<< :queue
+
+  # Runs the {#queue queued} {Requests}.
+  def run
+    fail 'Not implemented.'
+  end
 
 end
 
--- spec/raptor/protocol/http/client_spec.rb
+++ spec/raptor/protocol/http/client_spec.rb
@@ -1,4 +1,102 @@
-require_relative '../../../spec_helper'
+require 'spec_helper'
 
 describe Raptor::Protocol::HTTP::Client do
+  describe '#initialize' do
+    it 'sets the instance attributes by the options' do
+      options = { address: 'test.com', port: 81 }
+      client = described_class.new( options )
+
+      client.address.should == options[:address]
+      client.port.should == options[:port]
+    end
+    it 'uses 80 as the default port' do
+      described_class.new( address: 'stuff.com' ).port.should == 80
+    end
+
+    context 'when not passed an address' do
+      it 'raises an ArgumentError' do
+        raised = false
+        begin
+          described_class.new
+        rescue ArgumentError
+          raised = true
+        end
+        raised.should be_true
+      end
+    end
+  end
+
+  let(:client) { described_class.new( address: 'stuff.com' ) }
+
+  describe '#request' do
+    it 'forwards the given options to the Request object' do
+      options = { parameters: { 'name' => 'value' }}
+      client.request( '/blah/', options ).parameters.should == options[:parameters]
+    end
+
+    context 'when passed a block' do
+      it 'sets it as a callback' do
+        passed_response = nil
+        response = Raptor::Protocol::HTTP::Response.new
+
+        request = client.request( '/blah/' ) { |res| passed_response = res }
+        request.handle_response( response )
+        passed_response.should == response
+      end
+    end
+
+    it 'increments the queue size' do
+      client.queue_size.should == 0
+      client.request( '/blah/' )
+      client.queue_size.should == 1
+    end
+
+    it 'returns the request' do
+      client.request( '/blah/' ).should be_kind_of Raptor::Protocol::HTTP::Request
+    end
+  end
+
+  describe '#get' do
+    it 'queues a GET request' do
+      client.get( '/blah/' ).http_method.should == :get
+    end
+  end
+
+  describe '#post' do
+    it 'queues a POST request' do
+      client.post( '/blah/' ).http_method.should == :post
+    end
+  end
+
+  describe '#queue_size' do
+    it 'returns the amount of queued requests' do
+      client.queue_size.should == 0
+      10.times { client.request( '/' ) }
+      client.queue_size.should == 10
+    end
+  end
+
+  describe '#queue' do
+    it 'queues a request' do
+      client.queue_size.should == 0
+      client.queue( Raptor::Protocol::HTTP::Request.new )
+      client.queue_size.should == 1
+    end
+    it 'returns the queued request' do
+      request = Raptor::Protocol::HTTP::Request.new
+      client.queue(request).should == request
+    end
+  end
+
+  describe '#<<' do
+    it 'alias of #queue' do
+      client.queue_size.should == 0
+      client << Raptor::Protocol::HTTP::Request.new
+      client.queue_size.should == 1
+    end
+  end
+
+  describe '#run' do
+    it 'runs all the queued requests'
+  end
 end
