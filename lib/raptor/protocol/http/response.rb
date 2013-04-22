436d598ee0a3e3428f56a3eee92a91eb2200879c
Tasos Laskos <Tasos_Laskos@rapid7.com>
2013-04-22 22:40:21 +0300

    Protocol::HTTP::Response: added #code attribute

--- lib/raptor/protocol/http/response.rb
+++ lib/raptor/protocol/http/response.rb
@@ -8,8 +8,26 @@ module Protocol::HTTP
 #
 class Response < PDU
 
+  # @return [Integer] HTTP response status code.
+  attr_reader :code
+
   # @return [Request] HTTP {Request} which triggered this {Response}.
   attr_reader :request
+
+  #
+  # @note This class' options are in addition to {PDU#initialize}.
+  #
+  # @param  [Hash]  options Request options.
+  # @option options [Integer] :code HTTP response status code.
+  # @option options [Request] :request HTTP request that triggered this response.
+  #
+  # @see PDU#initialize
+  #
+  def initialize( options = {} )
+    super( options )
+
+    @code ||= 0
+  end
 
   # @return [String]
   #   String representation of the response, ready for HTTP transmission.
--- spec/raptor/protocol/http/response_spec.rb
+++ spec/raptor/protocol/http/response_spec.rb
@@ -3,6 +3,16 @@ require_relative '../../../spec_helper'
 describe Raptor::Protocol::HTTP::Response do
   it_should_behave_like 'Raptor::Protocol::HTTP::PDU'
 
+  describe '#code' do
+    it 'returns the HTTP status code' do
+      described_class.new( code: 200 ).code.should == 200
+    end
+
+    it 'defaults to 0' do
+      described_class.new.code.should == 0
+    end
+  end
+
   describe '#request' do
     it 'returns the assigned request' do
       r = Raptor::Protocol::HTTP::Request.new
