70dc168d52deb403a5ba386a0aed03fa16803b40
James Lee <egypt@metasploit.com>
2013-07-18 14:00:30 -0500

    Make TcpServer inherit from Tcp

--- lib/raptor/socket/tcp_server.rb
+++ lib/raptor/socket/tcp_server.rb
@@ -1,28 +1,4 @@
 # A listening TCP socket
-class Raptor::Socket::TcpServer < Raptor::Socket
-
-  # Factory method for creating a new {TcpServer} through the given
-  # {SwitchBoard `:switch_board`}
-  #
-  # @param (see Raptor::Socket::Comm.create)
-  # @option (see Raptor::Socket::Comm.create)
-  # @option opts :peer_host [String,IPAddr]
-  # @option opts :peer_port [Fixnum]
-  # @return [TcpServer]
-  def self.create(opts)
-    #required_keys = [ :local_host, :local_port ]
-    #validate_opts(opts, required_keys)
-
-    comm = opts[:switch_board].best_comm(opts[:peer_host])
-
-    # copy so we don't modify the caller's stuff
-    opts = opts.dup
-    opts[:proto] = :tcp
-    opts[:server] = true
-
-    sock = comm.create(opts)
-
-    new(sock)
-  end
+class Raptor::Socket::TcpServer < Raptor::Socket::Tcp
 
 end
