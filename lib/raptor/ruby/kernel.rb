2d462b1744978fa3b70b282ee623cc93de4e4d49
Tasos Laskos <Tasos_Laskos@rapid7.com>
2013-10-04 23:04:59 +0300

    Updated comments

--- lib/raptor/ruby/kernel.rb
+++ lib/raptor/ruby/kernel.rb
@@ -1,6 +1,7 @@
 module Kernel
 
-  # Wraps `IO.select` to compensate for some exentricities of OpenSSL::SSL::SSLSocket.
+  # Wraps `IO.select` to compensate for some exentricities of {OpenSSL::SSL::SSLSocket}.
+  # {OpenSSL::SSL::SSLSocket} whose buffer hasn't been consumed are returned immediately.
   def select( read = nil, write = nil, errors = nil, timeout = nil )
     if read.is_a? Array
       openssl_sockets = read.select { |s| s.is_a?( Raptor::Socket::TCP::SSL ) && !s.empty? }
--- lib/raptor/ruby/openssl.rb
+++ lib/raptor/ruby/openssl.rb
@@ -3,6 +3,19 @@ require 'openssl'
 module OpenSSL::SSL
 
 class SSLSocket
+
+  # OpenSSL does its own buffering which can result in a consumed TCP buffer,
+  # leading {Kernel.select} to think that the SSLSocket has no more data to
+  # provide, when that's not the case.
+  #
+  # Effectively making {Kernel.select} block forever, even though the SSLSocket's
+  # buffer has not yet been consumed by the caller.
+  #
+  # Thus, to make the {SSLSocket} behave similarly to `Socket`, we make use
+  # of this method along with a {Kernel.select} override to immediately return
+  # {SSLSocket}s which still have data to read.
+  #
+  # @private
   def empty?
     @rbuffer.empty?
   end
