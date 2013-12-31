ff8831567b30c5b3d3e947fe9175c18e0647d65e
David Maloney <DMaloney@rapid7.com>
2013-12-31 01:01:56 -0600

    tomcat bruteforce example
    sample showing simple version
    of the tomcat_mgr_login module

---
+++ samples/tomcat_mgr_login.rb
@@ -0,0 +1,59 @@
+#!/usr/bin/env ruby
+$:.push File.join(File.dirname(__FILE__), "..", "lib")
+
+require 'raptor'
+require 'optparse'
+
+options = {}
+
+myopts = OptionParser.new do |opts|
+  opts.banner = "Usage: example.rb [options]"
+
+  opts.on("--url <URL>", "Full URL of the Tomcat Mgr app to login to") do |url|
+    options[:url] = url
+  end
+
+  opts.on("--username <USERNAME>", "Username to attempt to login with") do |username|
+    options[:username] = username
+  end
+
+  opts.on("--password <PASSWORD>", "Password to attempt to login with") do |password|
+    options[:password] = password
+  end
+
+
+
+end
+
+myopts.parse!
+
+http_client =  Raptor::Protocol::HTTP::Client.new(switch_board: Raptor::Socket::SwitchBoard.new)
+
+puts "Sending Test request for Authentication challenge"
+test_response = http_client.get options[:url], mode: :sync
+
+if test_response.code != 401
+  puts "Did not request authorization. Make sure you have the URL right"
+  exit
+end
+
+get_opts = {
+  mode: :sync, manipulators: {
+    'authenticators/basic' =>
+    {
+      username: options[:username],
+      password: options[:password]
+    }
+  }
+}
+
+puts "Sending login #{options[:username]}:#{options[:password]}"
+tomcat_login_response =  http_client.get options[:url], get_opts
+
+
+if tomcat_login_response.code == 200
+  puts "LOGIN SUCCESSFUL! (#{options[:username]}:#{options[:password]})"
+else
+  puts "LOGIN FAILED (#{options[:username]}:#{options[:password]})"
+end
+
