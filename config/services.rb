860c3bfc58a83a3a99c7cbc6dc33d29333ff4416
Lance Sanchez <lance.sanchez@rapid7.com>
2014-03-12 14:17:04 -0500

    removing the extra server from services

--- config/services.rb
+++ config/services.rb
@@ -49,20 +49,3 @@ ServiceManager.define_service "client_close_connection" do |s|
   # The directory
   s.cwd        = Dir.pwd + "/spec/support/webservers/raptor/protocols/http/"
 end
-ServiceManager.define_service "basic" do |s|
-
-  # this is the host and port the service will be available on. If something is responding here, don't try to start it again because it's already running
-  s.host       = "localhost"
-  s.port       = 9294
-
-  s.start_cmd  = "ruby client_close_connection.rb -p #{s.port}"
-
-  # When this regexp is matches, ServiceManager will know that the service is ready
-  s.loaded_cue = /Listening for connections/
-
-  # ServiceManager will colorize the output as specified by this terminal color id.
-  s.color      = 36
-
-  # The directory
-  s.cwd        = Dir.pwd + "/spec/support/webservers/raptor/protocols/http/"
-end
